9aaf718555cf545b6683ac696e1ccaa71e60f6ac
Sean Hickman <sean@grumply.com>
2020-02-13 23:35:50 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure, pure-json, pure-variance, stdenv }:
 mkDerivation {
   pname = "pure-spacetime";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure pure-json pure-variance ];
   homepage = "github.com/grumply/pure-spacetime";
